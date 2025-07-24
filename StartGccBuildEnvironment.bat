a1b3fff1f37d37354175c7ef02eccf84b30a24fd
Schwabano <tim.zierer@gmail.com>
2025-07-24 17:54:29 -0600

    Update build environment from IMXRT1064-EVK-BuildEnvironment-V14.00.00.00.zip
    Uploaded 887 files on 2025-07-24 17:54:29

---
+++ StartGccBuildEnvironment.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: *************************************************************************************************
+:: Please do NOT change the following settings
+:: *************************************************************************************************
+set PlatformName=IMXRT1064-EVK
+set GettingStartedLink=getting-started-mimxrt1064-evk
+cmd /K Application\Project\GCC\devenv.cmd
---
+++ TargetSpecific/Drivers/display_support.h
@@ -0,0 +1,15 @@
+#ifndef _DISPLAY_SUPPORT_H_
+#define _DISPLAY_SUPPORT_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define DEMO_PANEL_RK043FN02H  0 /* RK043FN02H-CT */
+#define DEMO_PANEL_RK043FN66HS 1 /* RK043FN66HS-CTG */
+
+#ifndef DEMO_PANEL
+#define DEMO_PANEL DEMO_PANEL_RK043FN66HS
+#endif
+
+
+#endif /* _DISPLAY_SUPPORT_H_ */
